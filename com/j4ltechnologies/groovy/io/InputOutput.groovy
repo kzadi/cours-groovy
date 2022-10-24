fbeef22ba90359832f65bb907964b95f695e72c3
Joachim Zadi <joachim.zadi@j4ltechnologies.com>
2022-10-24 11:31:00 +0200

    io

---
+++ com/j4ltechnologies/groovy/io/InputOutput.groovy
@@ -0,0 +1,14 @@
+package com.j4ltechnologies.groovy.io
+
+// Entree Sortie
+print 'Saisir votre prenom : '
+def prenom = System.console().readLine()
+println "Votre prenom est $prenom"
+
+print 'Saisir votre âge : '
+def age = System.console().readLine().toInteger()
+println "Votre age est $age ans"
+
+print 'Saisir votre année de naissance : '
+def annee = System.console().readLine() as Integer
+println "Vous êtes née en $annee"
